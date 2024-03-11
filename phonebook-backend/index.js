ebbff63d143db2c6a7e13a54da7d10a921a1e5af
Матьё Роар <alapetite.mathieu@gmail.com>
2024-03-11 12:19:08 -0400

    3.1 - 3.2 pending (can't find Date)

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -9,6 +9,29 @@ app.get('/', (req, res) => {
     res.send('root')
 })
 
+app.get('/info', (req, res) => {
+    const nbrPersons = persons.length +1
+    console.log(nbrPersons)
+    // but how do you get the metadata from the response ?
+    res.send(`Phonebook has information on ${nbrPersons} persons.</br>Time of request : `)
+    
+    if (res.headersSent) {
+        console.log('in if statement');
+        (async() => {
+            try {
+                await (res.hasHeader('Date'))
+                console.log('has Date')
+                const headers = res.getHeaders()
+                console.log(JSON.stringify(headers))
+                // WTF ? how is this possible
+            } catch (err) {
+                console.log(err);
+            } 
+        })()
+        
+    } else {console.log(`No header`);}  
+})
+
 app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
