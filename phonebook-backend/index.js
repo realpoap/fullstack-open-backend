1d01c770d0cc0ec683bc2885a0c6e00a72cfb857
Матьё Роар <alapetite.mathieu@gmail.com>
2024-03-11 14:20:25 -0400

    3.3 - get single

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -32,6 +32,17 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    console.log(id);
+    const person = persons.find(p => p.id === id)
+    if (person) {
+        res.json(person)
+    } else { 
+        res.status(404).end() 
+    }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
