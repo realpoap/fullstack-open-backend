ef2850e5c7ffa8d8f5a53ea9f0e322d4a287bd35
Матьё Роар <alapetite.mathieu@gmail.com>
2024-03-20 13:46:09 -0400

    3.12 - mongo command-line db

---
+++ phonebook-backend/mongo.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url =
+  `mongodb+srv://poap:${password}@cluster-fullstack-open.ebaoy2d.mongodb.net/PhonebookApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+    const personSchema = new mongoose.Schema({
+        name: String,
+        number: String,
+      })
+      
+      const Person = mongoose.model('Person', personSchema)
+
+      if (!name || !number) {
+        Person.find({}).then(result => {
+            result.forEach(note => {
+              console.log(note)
+            })
+            mongoose.connection.close()
+          })
+    } else {
+        const person = new Person({
+            name: name,
+            number: number,
+        })
+  
+        person.save().then(result => {
+          console.log('person added!')
+          mongoose.connection.close()
+        })
+    }
+
+
+      
+      
+
