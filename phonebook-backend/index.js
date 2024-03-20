fe95e16140b4c516025953a0b5f33dd5cdd4b6d5
Матьё Роар <alapetite.mathieu@gmail.com>
2024-03-20 11:29:26 -0400

    3.9 - accessing backend

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
-// :method :url :status :res[content-length] - :response-time ms
+const cors = require('cors')
 
 let persons = require('./db.json')
 
@@ -10,6 +10,7 @@ morgan.token('content', function getContent (req) {
 })
 
 app.use(express.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('root')
@@ -78,7 +79,7 @@ app.post('/api/persons/', (req, res) => {
     res.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
